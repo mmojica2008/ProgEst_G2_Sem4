dffa22f1b368a701def83e763ca6d6b94ff3c921
Marcelo <mamojica@uamv.edu.ni>
2026-09-07 08:05:56 -0600

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,29 @@
+nombre_empresa = "TechNica"
+
+def mostrar_empresa():
+    print("Empresa:", nombre_empresa)
+
+mostrar_empresa()
+
+
+# EjercicioTotal
+# def calcular_total():
+#     total = 5000
+#     print("Total:", total)
+
+# calcular_total()
+
+
+
+# Contador Global
+# contador = 0
+
+# def aumentar_contador():
+#     global contador
+#     contador += 1
+
+# aumentar_contador()
+# aumentar_contador()
+# aumentar_contador()
+
+# print("Contador:", contador)
